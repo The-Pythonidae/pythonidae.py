19a852d5f1a18051712bd0d3b24bc82f4dd3b40f
Glitch <76911308+JBYT27@users.noreply.github.com>
2021-08-03 16:09:57 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+import os
+import time
+import json
+# Starter modules just in case
 
+def clear(): # Prob won't need this ;)
+  os.system('clear')
+  
+def cmd(command):
+  try:
+    os.system(str(command))
+  except:
+    exit()
+
+# Pretty much an example function for module. 
